de040852c1723ecc3e88bbf7b97719f8493039ec
Eric <eric.m.lombardo@gmail.com>
2021-06-21 10:59:11 -0400

    Build PostService class to handle fetch requests

---
+++ src/postService.js
@@ -0,0 +1,9 @@
+// make all service calls regarding a post object
+class PostService {
+  constructor(endpoint) {
+  this.endpoint = endpoint
+  }
+  getPosts() { // get request for all posts 
+  debugger;
+  }
+}
