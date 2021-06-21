61df91621964762279c3e4b9ce5f23b4aa043a74
Eric <eric.m.lombardo@gmail.com>
2021-06-21 12:50:22 -0400

    Collect other two divs from index.html

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const baseUrl = 'http://localhost:3000' // sets base url at global scope
 const postService = new PostService(baseUrl) // creates new postService to run requests
+const firstDiv = document.getElementById('first-div') // collect 1nd div
 const secondDiv = document.getElementById('second-div') // collect 2nd div
+const thirdDiv = document.getElementById('third-div') // collect 3rd div
 
 // get all posts
 postService.getPosts()
