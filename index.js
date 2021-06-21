b0b2df39b630e73061ee0e3f9785233be9a30c5b
Eric <eric.m.lombardo@gmail.com>
2021-06-21 11:53:00 -0400

    Add element / assign attributes in Post constructor

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const baseUrl = 'http://localhost:3000' // sets base url at global scope
 const postService = new PostService(baseUrl) // creates new postService to run requests
+
+// get all posts
 postService.getPosts()
--- src/post.js
+++ src/post.js
@@ -1,5 +1,6 @@
 class Post {
   static all = [] // class variable to remember all posts created
+  
   constructor(id, title, author, published, content, likes) {
     this.id = id                // assign attributes to this instance
     this.title = title
@@ -9,7 +10,13 @@ class Post {
     this.likes = likes  
 
     // create element for post
+    this.element = document.createElement('li')
     // assign identifiers
+    this.element.dataset.id = this.id
+    this.element.id = `post-${this.id}`
+
     Post.all.push(this)  // push `this` instance into all array
   }
+
+
 }
