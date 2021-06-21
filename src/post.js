123621f3983a28d6e96698674f78afc4a9212980
Eric <eric.m.lombardo@gmail.com>
2021-06-21 12:28:30 -0400

    Build additional functionality in Post class
    * Destructure class arguments comming into constructor
    * Build createPostHTML instance method
    * Build addPostToDom instance method

--- src/post.js
+++ src/post.js
@@ -1,7 +1,7 @@
 class Post {
   static all = [] // class variable to remember all posts created
   
-  constructor(id, title, author, published, content, likes) {
+  constructor({ id, title, author, published, content, likes }) {
     this.id = id                // assign attributes to this instance
     this.title = title
     this.author = author
@@ -10,7 +10,7 @@ class Post {
     this.likes = likes  
 
     // create element for post
-    this.element = document.createElement('li')
+    this.element = document.createElement('div')
     // assign identifiers
     this.element.dataset.id = this.id
     this.element.id = `post-${this.id}`
@@ -18,5 +18,16 @@ class Post {
     Post.all.push(this)  // push `this` instance into all array
   }
 
-
+  createPostHTML(){
+    this.element.innerHTML += `
+      <h3>${this.title}</h3>
+      <h4>${this.author}</h4>
+      <p>Published: ${this.published}</p>
+      <p>Likes: ${this.likes}</p>
+    `
+    return this.element
+  }
+  addPostToDom(){
+    secondDiv.appendChild(this.createPostHTML()) 
+  }
 }
