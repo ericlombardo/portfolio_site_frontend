d1e4dae36af0511007e64cd21ae8498206c7ea5e
Eric <eric.m.lombardo@gmail.com>
2021-06-21 12:27:30 -0400

    Collect secondDiv node from index.html

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const baseUrl = 'http://localhost:3000' // sets base url at global scope
 const postService = new PostService(baseUrl) // creates new postService to run requests
+const secondDiv = document.getElementById('second-div') // collect 2nd div
 
 // get all posts
 postService.getPosts()
