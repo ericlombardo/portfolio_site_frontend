7293f5393da525d664b380ef334fa75ae438c35a
Eric <eric.m.lombardo@gmail.com>
2021-06-21 17:22:43 -0400

    Remove like button functionality

--- src/post.js
+++ src/post.js
@@ -18,6 +18,7 @@ class Post {
     // assign identifiers
     this.element.dataset.id = this.id
     this.element.id = `post-${this.id}`
+    
 
     Post.all.push(this)  // push `this` instance into all array
   }
@@ -29,13 +30,13 @@ class Post {
       <h4>${this.author}</h4>
       <p>Published: ${this.published}</p>
       <p>Likes: ${this.likes}</p>
-      <img class="like-btn" src=${this.like_pic} alt="like button" width="20px">
     `
     return this.element
   }
 
   addPostToDom(){  // adds `this` instance to secondDiv
     secondDiv.appendChild(this.createPostHTML()) 
-    this.query
+
+
   }
 }
