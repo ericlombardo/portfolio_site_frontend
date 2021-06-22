54b94ea0d4f8da832369f2e7bdc55c9c96ee20be
Eric <eric.m.lombardo@gmail.com>
2021-06-22 12:30:31 -0400

    Build title generator in index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const firstDiv = document.getElementById('first-div') // collect 1nd div
 const secondDiv = document.getElementById('second-div') // collect 2nd div
 const thirdDiv = document.getElementById('third-div') // collect 3rd div
 
+// set title for main page
+generateTitle('Tech Blog')
 // get all posts
 postService.getPosts()
 
@@ -11,4 +13,13 @@ function clearPage() {
   firstDiv.innerHTML = ''
   secondDiv.innerHTML = ''
   thirdDiv.innerHTML = ''
+}
+
+function generateTitle(t) {
+  const title = `
+    <div class="title">
+      <h1>${t}</h1>
+    </div>
+  `
+  firstDiv.innerHTML += title
 }
