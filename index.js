8e45686e52e9f97ac138ba175506edc2c943ad98
Eric <eric.m.lombardo@gmail.com>
2021-06-23 21:02:48 -0400

    Build CommentService class and make new instance

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const baseUrl = 'http://localhost:3000' // sets base url at global scope
 const postService = new PostService(baseUrl) // creates new postService to run requests
+const commentService = new CommentService(baseUrl)
 const firstDiv = document.getElementById('first-div') // collect 1nd div
 const secondDiv = document.getElementById('second-div') // collect 2nd div
 const thirdDiv = document.getElementById('third-div') // collect 3rd div
 
 
 generateTitle('Tech Blog')  // set title for main page
+commentService.getComments() // get all comments
 postService.getPosts()  // get all posts
 
 // clear all divs on index.html
--- src/commentService.js
+++ src/commentService.js
@@ -1,1 +1,16 @@
 // make all service calls regarding a comment object
+class CommentService {
+  constructor(endpoint) {
+    this.endpoint = endpoint
+  }
+
+  getComments() {
+    fetch(this.endpoint + `/comments`)
+      .then(resp => resp.json())
+      .then(comments => {
+        for (const comment of comments) {
+          new Comment(comment)
+        }
+      })
+  }
+}
