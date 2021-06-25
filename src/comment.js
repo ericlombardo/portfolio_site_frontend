827d24776765eb31b44193068d052472c049ab73
Eric <eric.m.lombardo@gmail.com>
2021-06-25 13:01:37 -0400

    Start building Comment class

---
+++ src/comment.js
@@ -0,0 +1,9 @@
+class Comment {
+  constructor({ content, post_id, likes, like_pic, title_pic }) {
+    this.post_id = post_id
+    this.content = content
+    this.likes = likes
+    this.like_pic = like_pic
+    this.title_pic = title_pic
+  }
+}
