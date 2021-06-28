32cb9499d3484bdf6c36d64c7012597322044e87
Eric <eric.m.lombardo@gmail.com>
2021-06-28 10:55:11 -0400

    Set custom title and body font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+// tailwind.config.js
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    
+    fontFamily: {
+      title: ['Jura', 'sans-serif'],
+      body: ['Jura', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
