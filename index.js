6e8e3aaefe49ca740e607c084b3eac2c688a5e57
Eric <eric.m.lombardo@gmail.com>
2021-06-29 15:22:07 -0400

    Reformat index.js to align with new div structure

--- index.js
+++ index.js
@@ -1,33 +1,27 @@
 const baseUrl = 'http://localhost:3000' // sets base url at global scope
 const postService = new PostService(baseUrl) // creates new postService to run requests
 const commentService = new CommentService(baseUrl)
-const firstDiv = document.getElementById('first-div') // collect 1nd div
-const secondDiv = document.getElementById('second-div') // collect 2nd div
-const thirdDiv = document.getElementById('third-div') // collect 3rd div
+const blogTitle = document.getElementById('blog-title') // collect 1nd div
+const blogPosts = document.getElementById('blog-posts')
+
 
 function loadPage() {
-  generateTitle('Tech Blog')  // set title for main page
+  blogTitle.innerHTML = generateTitle('Tech Blog', "bg-night-computer")
   postService.getPosts()  // get all posts
 }
 
 // clear all divs on index.html
 function clearPage() {
-  firstDiv.innerHTML = ''
-  firstDiv.classList = ''
-  secondDiv.innerHTML = ''
-  secondDiv.classList = ''
-  thirdDiv.innerHTML = ''
-  thirdDiv.classList = ''
-
+  document.body.childNodes.forEach(el => el.innerHTML = '')
 }
-// generate title in firstDiv
-function generateTitle(t) {
+// generate title in titleDiv
+function generateTitle(t, backgroundClass) {
   const title = `
-    <div class="title bg-night-computer bg-cover bg-center bg-no-repeat py-20">
-      <h1 class="text-white text-7xl text-center font-title">${t}</h1>
-    </div>
+  <div class="w-screen title ${backgroundClass} bg-cover bg-center bg-no-repeat py-20">
+    <h1 class="text-white text-7xl text-center font-title">${t}</h1>
+  </div>
   `
-  firstDiv.innerHTML += title
+  return title
 }
 
 loadPage()
