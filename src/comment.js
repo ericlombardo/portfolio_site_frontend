89001bf82bcc46d40411bfc2d1490a63da45135a
Eric <eric.m.lombardo@gmail.com>
2021-06-30 14:27:14 -0400

    Remove comment attributes not needed

--- src/comment.js
+++ src/comment.js
@@ -1,10 +1,7 @@
 class Comment {
-  constructor({ content, post_id, likes, like_pic, title_pic }) {
+  constructor({ content, post_id}) {
     this.post_id = post_id
     this.content = content
-    this.likes = likes
-    this.like_pic = like_pic
-    this.title_pic = title_pic
   }
 
   createCommentHTML() { //create html that will show on post
