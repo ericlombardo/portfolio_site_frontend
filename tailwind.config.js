22504ad97b637eab84d4cea413349dd86101c87a
Eric <eric.m.lombardo@gmail.com>
2021-07-01 20:22:12 -0400

    Last commit before formatting tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,25 @@ module.exports = {
       }
     },
     extend: {
+      rotate: {
+        '20': '20deg',
+        '40': '40deg',
+        '60': '60deg',
+        '80': '80deg',
+        '100': '100deg',
+        '120': '120deg',
+        '140': '80deg',
+        '160': '90deg',
+        '180': '100deg',
+        '200': '110deg',
+        '220': '120deg',
+        '240': '130deg',
+        '260': '140deg',
+        '280': '280deg',
+        '300': '300deg',
+        '320': '320deg',
+        '340': '340deg'
+      },
       fontFamily: {
         'body': ['Jura', 'sans-serif'],
         'title': ['Play', 'sans-serif'],
