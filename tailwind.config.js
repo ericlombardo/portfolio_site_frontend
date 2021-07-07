b424c0a5589b1c125dcd87e0917cc0bd24d93f56
Eric <eric.m.lombardo@gmail.com>
2021-07-06 21:25:50 -0400

    Build landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,25 +23,6 @@ module.exports = {
       }
     },
     extend: {
-      rotate: {
-        '20': '20deg',
-        '40': '40deg',
-        '60': '60deg',
-        '80': '80deg',
-        '100': '100deg',
-        '120': '120deg',
-        '140': '80deg',
-        '160': '90deg',
-        '180': '100deg',
-        '200': '110deg',
-        '220': '120deg',
-        '240': '130deg',
-        '260': '140deg',
-        '280': '280deg',
-        '300': '300deg',
-        '320': '320deg',
-        '340': '340deg'
-      },
       fontFamily: {
         'body': ['Jura', 'sans-serif'],
         'title': ['Play', 'sans-serif'],
@@ -49,7 +30,74 @@ module.exports = {
       },
       backgroundImage: theme => ({
         'night-computer': "url(https://images.pexels.com/photos/3709434/pexels-photo-3709434.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=1280&w=1920)"
-      })
+      }),
+      animation: {
+        'rot10': 'rot10 30s linear infinite',
+        'rot20': 'rot20 30s linear infinite',
+        'rot30': 'rot30 30s linear infinite',
+        'rot40': 'rot40 30s linear infinite',
+        'rot50': 'rot50 30s linear infinite',
+        'rot60': 'rot60 30s linear infinite',
+        'rot70': 'rot70 30s linear infinite',
+        'rot80': 'rot80 30s linear infinite',
+        'rot90': 'rot90 30s linear infinite',
+      },
+      keyframes: {
+        rot10: {
+          '0%': {transform: 'rotate(0deg)'},
+          '5%': {transform: 'rotate(10deg)'},
+          '95%': {transform: 'rotate(10deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+        rot20: {
+          '0%': {transform: 'rotate(0deg)'},
+          '10%': {transform: 'rotate(20deg)'},
+          '90%': {transform: 'rotate(20deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+        rot30: {
+          '0%': {transform: 'rotate(0deg)'},
+          '15%': {transform: 'rotate(30deg)'},
+          '85%': {transform: 'rotate(30deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+        rot40: {
+          '0%': {transform: 'rotate(0deg)'},
+          '20%': {transform: 'rotate(40deg)'},
+          '80%': {transform: 'rotate(40deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+        rot50: {
+          '0%': {transform: 'rotate(0deg)'},
+          '25%': {transform: 'rotate(50deg)'},
+          '75%': {transform: 'rotate(50deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+        rot60: {
+          '0%': {transform: 'rotate(0deg)'},
+          '30%': {transform: 'rotate(60deg)'},
+          '70%': {transform: 'rotate(60deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+        rot70: {
+          '0%': {transform: 'rotate(0deg)'},
+          '35%': {transform: 'rotate(70deg)'},
+          '65%': {transform: 'rotate(70deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+        rot80: {
+          '0%': {transform: 'rotate(0deg)'},
+          '40%': {transform: 'rotate(80deg)'},
+          '60%': {transform: 'rotate(80deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+        rot90: {
+          '0%': {transform: 'rotate(0deg)'},
+          '45%': {transform: 'rotate(90deg)'},
+          '55%': {transform: 'rotate(90deg)'},
+          '100%': {transform: 'rotate(0deg)'},
+        },
+      }
     },
   },
   variants: {
