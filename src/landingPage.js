c8ec0abb13e65d529958a3d2a5eef711045214ea
Eric <eric.m.lombardo@gmail.com>
2021-07-06 21:30:15 -0400

    Remove extra file and add comments

--- src/landingPage.js
+++
@@ -1,9 +0,0 @@
-function getLanding() {
-  // const landingPage = document.getElementById('')
-  // landingPage.classList = 'w-screen h-screen bg-black  text-center flex items-center justify-items-center'
-  // landingPage.innerHTML = `
-    
-  //   <h1 class="text-7xl font-title text-teal block py-10">Eric Lombardo</h1>
-  //   <h2 class="text-5xl font-title text-teal block py-10">Software Developer</h2>
-  // `
-}
